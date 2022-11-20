31f37579de26f7f482942855ba929fe700ab107b
Kaho_s1280204 <79238271+Kaho8210@users.noreply.github.com>
2022-11-21 00:08:09 +0900

    Create Home.tsx

---
+++ ServiceEngineer/src/components/Home.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const Home = () => {
+  return (
+    <div>
+      <div style={{textAlign: 'center' , paddingTop: '180px'}}></div>
+        <h2>ホーム画面</h2>
+    </div>
+  )
+}
+
+export default Home;
